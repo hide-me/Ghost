ceef3b481f57f9c5e1e15e49dc4957664bf5dae7
Hannah Wolfe <github.erisds@gmail.com>
2024-08-21 11:49:30 +0100

    Updated charts to respect the number of selected days
    - Analytics charts now show 7/30/90 days of data as per the dropdown

--- ghost/admin/app/components/dashboard/charts/kpis.js
+++ ghost/admin/app/components/dashboard/charts/kpis.js
@@ -3,26 +3,49 @@
 import Component from '@glimmer/component';
 import React from 'react';
 import config from 'ghost-admin/config/environment';
+import moment from 'moment-timezone';
 import {AreaChart} from '@tinybirdco/charts';
 
-export default class Kpis extends Component {
-    /**
-     * @param {Object} params
-     * @param {string} params.cid
-     * @param {string} [params.date_from]
-     * @param {string} [params.date_to]
-     * @returns {JSX.Element}
-     */
-    ReactComponent = (params) => {
+export default class KpisComponent extends Component {
+    ReactComponent = (props) => {
+        let chartDays = props.chartDays;
+
+        const endDate = moment().endOf('day');
+        const startDate = moment().subtract(chartDays - 1, 'days').startOf('day');
+
+        /**
+         * @typedef {Object} Params
+         * @property {string} cid
+         * @property {string} [date_from]
+         * @property {string} [date_to]
+         * @property {number} [limit]
+         * @property {number} [skip]
+         */
+        const params = {
+            cid: config.tinybirdCid,
+            date_from: startDate.format('YYYY-MM-DD'),
+            date_to: endDate.format('YYYY-MM-DD')
+        };
+
         return (
             <AreaChart
-                endpoint={ 'https://api.tinybird.co/v0/pipes/kpis.json?cid=' + config.tinybirdCid }
+                endpoint="https://api.tinybird.co/v0/pipes/kpis.json"
                 token={config.tinybirdToken}
                 index="date"
                 categories={['visits']}
                 colorPalette={['#DC82C8', '#FFC0F1']}
                 height="300px"
                 params={params}
+                options={{
+                    xAxis: {
+                        type: 'time',
+                        min: startDate.toISOString(),
+                        max: endDate.toISOString(),
+                        axisLabel: {
+                            formatter: chartDays <= 7 ? '{ee}' : '{dd} {MMM}'
+                        }
+                    }
+                }}
             />
         );
     };
--- ghost/admin/app/components/dashboard/charts/top-pages.js
+++ ghost/admin/app/components/dashboard/charts/top-pages.js
@@ -3,6 +3,7 @@
 import Component from '@glimmer/component';
 import React from 'react';
 import config from 'ghost-admin/config/environment';
+import moment from 'moment-timezone';
 import {BarList} from '@tinybirdco/charts';
 
 export default class TopPages extends Component {
@@ -13,7 +14,26 @@ export default class TopPages extends Component {
      * @param {string} [params.date_to]
      * @returns {JSX.Element}
      */
-    ReactComponent = (params) => {
+    ReactComponent = (props) => {
+        let chartDays = props.chartDays;
+
+        const endDate = moment().endOf('day');
+        const startDate = moment().subtract(chartDays - 1, 'days').startOf('day');
+
+        /**
+         * @typedef {Object} Params
+         * @property {string} cid
+         * @property {string} [date_from]
+         * @property {string} [date_to]
+         * @property {number} [limit]
+         * @property {number} [skip]
+         */
+        const params = {
+            cid: config.tinybirdCid,
+            date_from: startDate.format('YYYY-MM-DD'),
+            date_to: endDate.format('YYYY-MM-DD')
+        };
+
         return (
             <BarList
                 endpoint={ 'https://api.tinybird.co/v0/pipes/top_pages.json?cid=' + config.tinybirdCid }
